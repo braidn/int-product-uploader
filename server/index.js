e69038cd89c59b8a4c7176dee38c4045a55f22d8
Braden Douglass <braden.douglass@gmail.com>
2022-09-14 01:50:39 -0400

    Separate Application From Server.
    Aligning the express work here more with what is generated from
    express-generator. This separates the app logic away from the server
    logic. This also aids in testability in the server.

--- server/index.js
+++ server/index.js
@@ -1,15 +1,62 @@
-import express from 'express'
-import cors from 'cors'
+/**
+ * Module dependencies.
+ */
 
-const app = express()
+import app from './app.js'
+import http from 'http'
+import debugLib from 'debug'
+const debug = debugLib('accounts:server')
 
-app.use(cors())
+/**
+ * Get port from environment and store in Express.
+ */
+var port = process.env.PORT || '3000'
+app.set('port', port)
 
-app.get('/', (req, res) => {
-  res.send('This is from express.js')
-})
+/**
+ * Create HTTP server.
+ */
+var server = http.createServer(app)
 
-const port = process.env.PORT || 5000
-app.listen(port, () => {
-  console.log(`server started on port ${port}: http://localhost:${port}`)
-})
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port)
+server.on('error', onError)
+server.on('listening', onListening)
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error
+  }
+
+  var bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges')
+      process.exit(1)
+      break
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use')
+      process.exit(1)
+      break
+    default:
+      throw error
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address()
+  var bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port
+  debug('Listening on ' + bind)
+}
